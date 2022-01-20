bbe069564a5209aa7c0264bfc5c722a527d1a0fe
Diogo Jesus <diogojesus92@ua.pt>
2022-01-20 22:38:22 +0000

    Alteração do ClientRequest para remover o @JsonProperty

--- garage-api-test/src/main/java/api/mappings/garage/client/ClientRequest.java
+++ garage-api-test/src/main/java/api/mappings/garage/client/ClientRequest.java
@@ -6,26 +6,15 @@ import java.util.Date;
 
 public class ClientRequest {
 
-    @JsonProperty("id")
     private Integer id;
-    @JsonProperty("firstName")
     private String firstName;
-    @JsonProperty("lastName")
     private String lastName;
-    @JsonProperty("address")
     private String address;
-    @JsonProperty("postalCode")
     private String postalCode;
-    @JsonProperty("city")
     private String city;
-    @JsonProperty("country")
     private String country;
-    @JsonProperty("phoneNumber")
     private Integer phoneNumber;
-    @JsonProperty("nif")
     private String nif;
-    @JsonProperty("birthDate")
     private Date birthDate;
-    @JsonProperty("clientDate")
     private Date clientDate;
 }
