1008bdf1c9e8b88e3910df8edf5707c5195cde54
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-27 22:03:54 +0000

    [upd] delete vehicle negative tests

--- garage-api-test/src/test/java/api/garage/vehicle/DeleteVehicleNegativeTests.java
+++ garage-api-test/src/test/java/api/garage/vehicle/DeleteVehicleNegativeTests.java
@@ -10,6 +10,7 @@ import retrofit2.Response;
 
 import java.io.IOException;
 
+import static api.garage.helper.ErrorVehicleResponses.errorVehicleInvalidID;
 import static api.garage.helper.ErrorVehicleResponses.errorVehicleNotFound;
 import static api.garage.helper.VehicleResquests.vehiclePositive;
 import static api.retrofit.garage.Error.getErrorResponse;
@@ -39,20 +40,37 @@ public class DeleteVehicleNegativeTests {
         Response<Integer> createResponse = Vehicle.createVehicle(vehicle);
         assertCreated(createResponse);
         createdVehicleID = createResponse.body();
-        Integer idToTest = createdVehicleID;
-        assertIDNotNull(idToTest);
+        Integer vehicleIDToTest = createdVehicleID;
+        assertIDNotNull(vehicleIDToTest);
 
-        Response<Void> response = Vehicle.deleteVehicleByID(idToTest);
+        Response<Void> response = Vehicle.deleteVehicleByID(vehicleIDToTest);
         assertNoContent(response);
         createdVehicleID = null;
 
-        Response<Void> getResponse = Vehicle.deleteVehicleByID(idToTest);
+        Response<Void> getResponse = Vehicle.deleteVehicleByID(vehicleIDToTest);
         assertNotFound(getResponse);
 
-        ErrorResponse expectedResponse = errorVehicleNotFound(idToTest);
+        ErrorResponse expectedResponse = errorVehicleNotFound(vehicleIDToTest);
         assertErrorResponse(getErrorResponse(getResponse), expectedResponse);
     }
 
-    // delete vehicle duas vezes (esperar not found)
-    // delete vehicle 0 e negativo e esperar bad request
+    @Test(description = "ID: GT0001")
+    public void deleteVehicleWithID0Test() throws IOException {
+        Integer vehicleIDToTest = 0;
+        Response<Void> getResponse = Vehicle.deleteVehicleByID(vehicleIDToTest);
+        assertBadRequest(getResponse);
+
+        ErrorResponse expectedResponse = errorVehicleInvalidID(vehicleIDToTest);
+        assertErrorResponse(getErrorResponse(getResponse), expectedResponse);
+    }
+
+    @Test(description = "ID: GT0001")
+    public void deleteVehicleWithNegativeIDTest() throws IOException {
+        Integer vehicleIDToTest = -100;
+        Response<Void> getResponse = Vehicle.deleteVehicleByID(vehicleIDToTest);
+        assertBadRequest(getResponse);
+
+        ErrorResponse expectedResponse = errorVehicleInvalidID(vehicleIDToTest);
+        assertErrorResponse(getErrorResponse(getResponse), expectedResponse);
+    }
 }
