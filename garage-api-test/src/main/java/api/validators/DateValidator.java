30afc3cb5a06f52d2fdd5a34fa78854faf5ad273
Gonçalo Marques <53789763+Goncalo-Marques@users.noreply.github.com>
2022-01-27 22:55:08 +0000

    [new] date validator

---
+++ garage-api-test/src/main/java/api/validators/DateValidator.java
@@ -0,0 +1,30 @@
+package api.validators;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class DateValidator {
+    public static void assertDatelessEqualTo(LocalDate actual, LocalDate expected) {
+        if (expected == null) {
+            assertThat("Date should be null", actual, nullValue());
+            return;
+        } else {
+            assertThat("Date should not be null", actual, notNullValue());
+        }
+
+        assertThat("Date is not the expected", actual, is(expected));
+    }
+
+    public static void assertDatelessThanOrEqualTo(LocalDate actual, LocalDate expected) {
+        if (expected == null) {
+            assertThat("Date should be null", actual, nullValue());
+            return;
+        } else {
+            assertThat("Date should not be null", actual, notNullValue());
+        }
+
+        assertThat("Date is not the expected", (actual.isBefore(expected) || actual.isEqual(expected)));
+    }
+}
